76f45db3ec8e9631fdbda0f2fedcab501c23f452
Solotona <solotona@mail.ru>
2026-02-08 19:33:46 +0300

    Added the test 'Get booking by ID'

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -1,10 +1,15 @@
+from http.client import responses
+
 import requests
 import os # встроенный модуль для работы с файлами
 from dotenv import load_dotenv # устанавливаем библиотеку python-dotenv. Функция load_dotenv проводит анализ файла .env, а затем загружает все найденные переменные в качестве переменных окружения""""
 from core.settings.environments import Environment
+from core.clients.endpoints import Endpoints
+from core.settings.config import Users, Timeouts
+
+import allure
 
 load_dotenv()
-
 
 # создаем класс APIClient — обёртку для работы с внешними сервисами через API
 class APIClient:
@@ -22,8 +27,11 @@ class APIClient:
         # 3. Устанавливаем базовый URL в зависимости от среды
         self.base_url = self.get_base_url(environment)
 
+        # 4. Сздаём объект сессии из библиотеки requests, который используется для выполнения HTTP-запросов: сессия сохраняет соединения между запросами, сессия автоматически сохраняет и отправляет куки между запросами
+        self.session = requests.Session()
+
         # 4. Задаём стандартные заголовки по умолчанию для всех запросов (в данном случае — формат данных application/json).
-        self.headers = {
+        self.session.headers = {
         'Content-Type': 'application/json'
         }
 
@@ -41,7 +49,7 @@ class APIClient:
 
     def get(self, endpoint, params=None, statusCode=200):
         url = self.base_url + endpoint
-        response = requests.get(url, headers=self.headers, params=params)
+        response = requests.get(url, headers=self.session.headers, params=params)
         if statusCode:
             assert response.status_code == statusCode
         return response.json() # Метод response.json() в Python (обычно используемый в библиотеке requests) преобразует полученный от сервера ответ в формате JSON (строку) в удобные для работы структуры данных Python — словари (dict) или списки (list). Он автоматически декодирует JSON-контент, позволяя сразу обращаться к данным
@@ -52,3 +60,32 @@ class APIClient:
         if statusCode:
             assert response.status_code == statusCode
         return response.json()
+
+    # Создаем метод класса, который предназначен для проверки доступности («пинга») API.
+    def ping(self):
+        with allure.step('Ping api client'):
+            url = f"{self.base_url}{Endpoints.PING_ENDPOINT}"
+            response = self.session.get(url)
+            response.raise_for_status() # Функция response.raise_for_status() в библиотеке requests нужна для автоматической проверки успешности HTTP-запроса. Если сервер вернул код ошибки (4xx или 5xx, например, 404 или 500), функция вызывает исключение requests.exceptions.HTTPError. Это предотвращает дальнейшую обработку ошибочных данных
+        with allure.step('Assert status code'):
+            assert response.status_code == 201, f"Expected status 201 but got {response.status_code}"
+        return response.status_code
+
+    # Создаем метод auth, который выполняет аутентификацию пользователя и сохраняет токен для последующих запросов
+    def auth(self):
+        with allure.step('Getting authenticate'):
+            url = f"{self.base_url}{Endpoints.AUTH_ENDPOINT}" # Формирует полный URL для запроса, объединяя базовый адрес (self.base_url) и путь к эндпоинту аутентификации (Endpoints.AUTH_ENDPOINT)
+            payload = {"username": Users.USERNAME, "password": Users.PASSWORD} # Создаёт тело запроса (payload) с учётными данными из класса Users
+            response = self.session.post(url, json = payload, timeout = Timeouts.TIMEOUT) # Отправляет POST-запрос через сессию (self.session) с JSON-данными и заданным таймаутом
+            response.raise_for_status()  # Функция response.raise_for_status() в библиотеке requests нужна для автоматической проверки успешности HTTP-запроса. Если сервер вернул код ошибки (4xx или 5xx, например, 404 или 500), функция вызывает исключение requests.exceptions.HTTPError. Это предотвращает дальнейшую обработку ошибочных данных
+            with allure.step('Checking status code'):
+                assert response.status_code == 200, f"Expected status 200 but got {response.status_code}"
+            token = response.json().get("token") # Парсит JSON-ответ и извлекает значение поля "token"
+            with allure.step('Updating header with authorization'):
+                self.session.headers.update({"Authorization": f"Bearer {token}"}) # Добавляет заголовок авторизации в сессию, чтобы все последующие запросы автоматически включали токен
+
+    def get_booking_by_id(self, booking_id: int):
+        with allure.step('Getting booking by ID'):
+            response = self.session.get(f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}/{booking_id}", timeout=Timeouts.TIMEOUT)
+            response.raise_for_status()
+            assert response.status_code == 200, f"Expected 200 but got {response.status_code}"
