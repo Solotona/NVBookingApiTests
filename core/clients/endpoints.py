c1679237bfadfbb5e956ba726c98ec3cbeaf958c
Solotona <solotona@mail.ru>
2026-02-10 21:37:10 +0300

    Added the test 'Get booking by ID'

---
+++ core/clients/endpoints.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+# Endpoints как класс со строковыми константами
+class Endpoints:
+    PING_ENDPOINT = "/ping"
+    AUTH_ENDPOINT = "/auth"
+    BOOKING_ENDPOINT = "/booking"
